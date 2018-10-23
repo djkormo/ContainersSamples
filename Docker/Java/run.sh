3c98e67315b3de82ed666bac58de0560ccb4c715
Krzysztof Pudłowski <kpudlowski@plus.pl>
2018-10-23 13:51:51 +0200

    Fixing Java/run.sh

--- Docker/Java/run.sh
+++ Docker/Java/run.sh
@@ -1,4 +1,8 @@
+# To build the image, start with the following command:
+$ docker build -f Dockerfile -t demo/oracle-java:8 .
+
 #To compile your Main.java file.
 $ docker run --rm -v $PWD:/app -w /app demo/oracle-java:8 javac Main.java
+
 #To run your compiled Main.class file.
 $ docker run --rm -v $PWD:/app -w /app demo/oracle-java:8 java Main
