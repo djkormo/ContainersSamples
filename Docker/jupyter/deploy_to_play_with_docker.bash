a2b12c864c5755a5eb055788564bc89150ce2736
Twoje imię i nazwsko <djkormo@gmail.com>
2019-04-28 19:19:33 +0200

    adding sleep to Docker/jupyter/deploy_to_play_with_docker.bash

--- Docker/jupyter/deploy_to_play_with_docker.bash
+++ Docker/jupyter/deploy_to_play_with_docker.bash
@@ -4,6 +4,8 @@ docker build -t minimal-jupyter .
 # run 
 docker run -p 8888:8888 -d minimal-jupyter
 
+sleep 15s
+
 # displaying logs
 
 docker logs $(docker ps -q --filter ancestor=minimal-jupyter)
