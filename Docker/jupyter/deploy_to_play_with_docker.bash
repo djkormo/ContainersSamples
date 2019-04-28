ed0417d1e8ca2d34d3c4cde42418280228053e11
Twoje imię i nazwsko <djkormo@gmail.com>
2019-04-28 22:28:05 +0200

    adding Docker/jupyter/deploy_to_play_with_docker.bash

--- Docker/jupyter/deploy_to_play_with_docker.bash
+++ Docker/jupyter/deploy_to_play_with_docker.bash
@@ -2,12 +2,13 @@
 # build 
 docker build -t minimal-jupyter .
 # run 
-docker run -p 8888:8888 -d -v ~/notebooks:/home/jovyan minimal-jupyter
+
+docker run -p 8888:8888 -d --mount source=notebooks,target=/home/jovyan/notebooks  minimal-jupyter
+
 
 sleep 15s
 
 # displaying logs
 
 docker logs $(docker ps -q --filter ancestor=minimal-jupyter)
-
 
