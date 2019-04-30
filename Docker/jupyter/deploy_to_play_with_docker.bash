618648507e19b85311fcf8a8ed27956b265ac8e2
Twoje imię i nazwsko <djkormo@gmail.com>
2019-04-30 22:57:58 +0200

    adding permission to mount

--- Docker/jupyter/deploy_to_play_with_docker.bash
+++ Docker/jupyter/deploy_to_play_with_docker.bash
@@ -1,9 +1,14 @@
 #!/bin/bash
 # build 
 docker build -t minimal-jupyter .
+
+# permission on host directory
+sudo chown -R 1000 /root/ContainersSamples
+
+
 # run 
 
-docker run -p 8888:8888 -d --mount source=notebooks,target=/home/jovyan/notebooks  minimal-jupyter
+docker run -p 8888:8888 -d -v /root/ContainersSamples/Docker/jupyter/notebooks:/home/jovyan/notebooks  minimal-jupyter
 
 
 sleep 15s
--- Docker/jupyter/deploy_to_play_with_docker_no_token.bash
+++ Docker/jupyter/deploy_to_play_with_docker_no_token.bash
@@ -1,10 +1,16 @@
 #!/bin/bash
 # build 
 docker build -t minimal-jupyter .
+
+
+sudo chown -R 1000 /root/ContainersSamples
+
 # run 
 
 #docker run -p 8888:8888 -d --mount source=notebooks,target=/home/jovyan/notebooks  minimal-jupyter
-docker run -d -p 8888:8888 minimal-jupyterstart-notebook.sh --NotebookApp.token=''
+docker run -d -p 8888:8888 minimal-jupyter start-notebook.sh --NotebookApp.token=''
+
+docker run -d -p 8888:8888 -v /root/ContainersSamples/Docker/jupyter/notebooks:/home/jovyan/notebooks minimal-jupyter start-notebook.sh --NotebookApp.token=''
 
 sleep 15s
 
