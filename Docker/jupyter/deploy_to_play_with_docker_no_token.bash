5ceb8860ae771e8de08ddb49a2ffaf625ab3f99a
Twoje imię i nazwsko <djkormo@gmail.com>
2019-04-30 23:24:33 +0200

    correcting Docker/jupyter/deploy_to_play_with_docker_no_token.bash

--- Docker/jupyter/deploy_to_play_with_docker_no_token.bash
+++ Docker/jupyter/deploy_to_play_with_docker_no_token.bash
@@ -2,13 +2,12 @@
 # build 
 docker build -t minimal-jupyter .
 
+# permissions 
 
 sudo chown -R 1000 /root/ContainersSamples
 
 # run 
 
-#docker run -p 8888:8888 -d --mount source=notebooks,target=/home/jovyan/notebooks  minimal-jupyter
-docker run -d -p 8888:8888 minimal-jupyter start-notebook.sh --NotebookApp.token=''
 
 docker run -d -p 8888:8888 -v /root/ContainersSamples/Docker/jupyter/notebooks:/home/jovyan/notebooks minimal-jupyter start-notebook.sh --NotebookApp.token=''
 
