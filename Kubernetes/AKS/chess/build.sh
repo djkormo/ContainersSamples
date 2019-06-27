e3d988ad352b404d70c40669f058b637cf197505
djkormo <kpudlowski@plus.pl>
2019-06-27 10:07:36 +0200

    correcting build.sh

--- Kubernetes/AKS/chess/build.sh
+++ Kubernetes/AKS/chess/build.sh
@@ -8,6 +8,13 @@ DOCKER_IMAGE_REPO_NAME=$(DOCKER_REGISTRY)/$(DOCKER_IMAGE_NAME)
 
 #  build 
 docker build -t chess-ai .
-docker build -t chess-ai:blue . -f Docker-blue 
-docker build -t chess-ai:green . -f Docker-green
+docker build -t chess-ai:blue . -f Dockerfile-blue 
+docker build -t chess-ai:green . -f Dockerfile-green
 
+
+docker tag chess-ai:blue djkormo/chess-ai:blue
+docker tag chess-ai:green djkormo/chess-ai:green
+
+
+docker push  djkormo/chess-ai:blue
+docker push  djkormo/chess-ai:green
