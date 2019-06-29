b92917cd39347f068557eeaff24e81d2ade4cedd
djkormo <djkormo@gmail.com>
2019-06-29 16:18:54 +0200

    adding rbac-config.yaml

--- Kubernetes/AKS/simple/run_minikube_helm.sh
+++ Kubernetes/AKS/simple/run_minikube_helm.sh
@@ -16,3 +16,30 @@ helm version
 
 # list all pods in kube-system namespace 
 kubectl get pods --namespace kube-system
+
+
+
+helm search grafana
+
+helm inspect stable/grafana
+
+helm install stable/grafana --name mygrafana --set service.type=NodePort
+
+helm status mygrafana
+
+kubectl get pods
+
+kubectl get secret --namespace default mygrafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+
+
+export NODE_PORT=$(sudo kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services mygrafana)
+
+export NODE_IP=$(sudo kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
+
+echo http://$NODE_IP:$NODE_PORT
+
+
+
+sudo helm list
+
+
