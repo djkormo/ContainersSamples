beab5a69b13c305f8b37dd542388776fc87042d2
djkormo <kpudlowski@plus.pl>
2019-07-04 14:08:41 +0200

    adding loadtest application

---
+++ Kubernetes/AKS/k8s-deployment-strategies/apps/loadtest/build.sh
@@ -0,0 +1,13 @@
+DOCKER_REGISTRY=docker.io
+DOCKER_PROJECT_ID=djkormo
+SERVICE_NAME=loadtest
+DOCKER_IMAGE_NAME=$(DOCKER_PROJECT_ID)/${SERVICE_NAME}
+DOCKER_IMAGE_REPO_NAME=$(DOCKER_REGISTRY)/$(DOCKER_IMAGE_NAME)
+
+#  build 
+docker build -t loadtest .
+# tag
+docker tag k8s-dep-strat djkormo/loadtest
+
+#push
+docker push  djkormo/loadtest
