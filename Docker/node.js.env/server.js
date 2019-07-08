c348af3cb3c1d7a7f36e4da0feed585d5b4ccb7c
Twoje imię i nazwsko <djkormo@gmail.com>
2019-07-09 00:19:02 +0200

    adding hardcode.js

--- Docker/node.js.env/server.js
+++ Docker/node.js.env/server.js
@@ -5,11 +5,16 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
+const language = 'English';
+const API_KEY = '123-456-789';
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
+  
+  response.write(`Language: ${language}\n`);
+  response.write(`API Key: ${API_KEY}\n`);
+response.end(`\n`);
 });
 
 app.listen(PORT, HOST);
