757ffddee33d4e3c7b37736089404082e8011230
Twoje imię i nazwsko <djkormo@gmail.com>
2019-07-09 00:19:54 +0200

    adding envvars.js

---
+++ Docker/node.js.env/envvars.js
@@ -0,0 +1,11 @@
+// Copyright 2017, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License")
+var http = require('http');
+var server = http.createServer(function (request, response) {
+  const language = process.env.LANGUAGE;
+  const API_KEY = process.env.API_KEY;
+  response.write(`Language: ${language}\n`);
+  response.write(`API Key: ${API_KEY}\n`);
+  response.end(`\n`);
+});
+server.listen(3000);
