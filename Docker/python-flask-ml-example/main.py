9e4f24ca6594db27e2480e3aae04df69d3f19a79
djkormo <kpudlowski@plus.pl>
2019-07-09 13:20:19 +0200

    correcting python-flask-ml-example/main.py

--- Docker/python-flask-ml-example/main.py
+++ Docker/python-flask-ml-example/main.py
@@ -26,33 +26,19 @@ ns = api.namespace('', description='Operations for Regression API')
 SWAGGER_URL = '/api/docs' # URL for exposing Swagger UI (without trailing '/')
 API_URL = 'http://petstore.swagger.io/v2/swagger.json' # Our API url (can of course be a local resource)
 
-# Call factory function to create our blueprint
-#swaggerui_blueprint = get_swaggerui_blueprint(
-#SWAGGER_URL, # Swagger UI static files will be #mapped to '{SWAGGER_URL}/dist/'
-#API_URL,
-#config={ # Swagger UI config overrides
-#'app_name': "Test application"
-#},
-# oauth_config={ # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
-# 'clientId': "your-client-id",
-# 'clientSecret': "your-client-secret-if-required",
-# 'realm': "your-realms",
-# 'appName': "your-app-name",
-# 'scopeSeparator': " ",
-# 'additionalQueryStringParams': {'test': "hello"}
-# }
-)
-
-# Register blueprint at URL
-# (URL must match the one given to factory function above)
-#app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+@ns.route('/health')  #  Create a URL route to this resource
+class HelloWorld(Resource):            #  Create a RESTful resource
+  def get(self):                     #  Create GET endpoint
+    #return {'hello': 'world'}
+    return '', 200'
+	
 
 # loading a model from a file called model.pkl
 with open("model.pkl", "rb") as fr:
     model = load(fr)
 
 
-@app.route("/abc")
+@ns.route("/abc")
 @cross_origin() # allow all origins all methods.
 def helloWorld():
   return "Hello, cross-origin-world!"    
