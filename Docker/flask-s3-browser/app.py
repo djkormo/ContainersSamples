c0a3ca24e24f90967aa0376c4bdc4e6d43e889ca
Krzysztof Pudłowski <kpudlowski@plus.pl>
2021-03-17 13:25:34 +0100

    initial version of flask-s3-browser

---
+++ Docker/flask-s3-browser/app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, \
+    Response, session
+from flask_bootstrap import Bootstrap
+from filters import datetimeformat, file_type
+from resources import get_bucket, get_buckets_list
+
+app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = 'secret'
+app.jinja_env.filters['datetimeformat'] = datetimeformat
+app.jinja_env.filters['file_type'] = file_type
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        bucket = request.form['bucket']
+        session['bucket'] = bucket
+        return redirect(url_for('files'))
+    else:
+        buckets = get_buckets_list()
+        return render_template("index.html", buckets=buckets)
+
+
+@app.route('/files')
+def files():
+    my_bucket = get_bucket()
+    summaries = my_bucket.objects.all()
+
+    return render_template('files.html', my_bucket=my_bucket, files=summaries)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+
+    my_bucket = get_bucket()
+    my_bucket.Object(file.filename).put(Body=file)
+
+    flash('File uploaded successfully')
+    return redirect(url_for('files'))
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    key = request.form['key']
+
+    my_bucket = get_bucket()
+    my_bucket.Object(key).delete()
+
+    flash('File deleted successfully')
+    return redirect(url_for('files'))
+
+
+@app.route('/download', methods=['POST'])
+def download():
+    key = request.form['key']
+
+    my_bucket = get_bucket()
+    file_obj = my_bucket.Object(key).get()
+
+    return Response(
+        file_obj['Body'].read(),
+        mimetype='text/plain',
+        headers={"Content-Disposition": "attachment;filename={}".format(key)}
+    )
+
+
+if __name__ == "__main__":
+    app.run()
